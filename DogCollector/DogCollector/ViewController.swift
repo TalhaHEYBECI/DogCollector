47573489e0ee3c61f50aa92fe2adc49efb21ab77
talha heybeci <aheybeci@hotmail.com>
2024-07-21 14:22:38 +0300

    first commit

---
+++ DogCollector/DogCollector/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DogCollector
+//
+//  Created by talha heybeci on 21.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
